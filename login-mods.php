90e94d61316dd6c01949da3009de375d6e1da620
S H Mohanjith <moha@mohanjith.net>
2014-07-14 12:50:02 +0300

    Author fix

--- login-mods.php
+++ login-mods.php
@@ -3,11 +3,29 @@
 Plugin Name: Log In Message
 Plugin URI:
 Description: Add custom log in messages
-Author: S H Mohanjith (Incsub)
+Author: WPMU DEV
 Version: 1.0.2
 Network: true
 WDP ID: 256
 Text Domain: login_mods
+*/
+
+/*
+Copyright 2007-2014 Incsub (http://incsub.com)
+Author - S H Mohanjith (Incsub)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 define('LOGIN_MODS_VERSION', '1.0.2');
